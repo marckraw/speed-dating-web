b9472e3cebe04c9cadf2d64a6c5b917477dd4f84
Marcin Krawczyk <marckraw@icloud.com>
2025-06-13 20:50:01 +0200

    fix: wypierdolony react-use effect i collapsible z radixa

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -7,13 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  transpilePackages: ["@radix-ui/react-use-effect-event"],
   webpack(config) {
-    config.resolve.alias["@radix-ui/react-use-effect-event"] = path.resolve(
-      __dirname,
-      "src/stubs/use-effect-event.js"
-    );
-
     // Explicitly set up the @ alias
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
 

e38778ce2e95c90928d0a9b6c4f43c8926ef3433
Marcin Krawczyk <marckraw@icloud.com>
2025-06-13 20:41:19 +0200

    fix: turbo and remove db from file

--- packages/backend/src/scripts/sql-scripts.ts
+++ packages/backend/src/scripts/sql-scripts.ts
@@ -1,10 +1,8 @@
 import "dotenv/config";
-import { pool } from "../db";
 
 async function run() {
   // await pool.query(`ALTER TABLE habits ADD COLUMN type TEXT DEFAULT 'boolean'`); - some example sql script
   console.log("✅ Column added successfully");
-  await pool.end();
 }
 
 run().catch(console.error);
